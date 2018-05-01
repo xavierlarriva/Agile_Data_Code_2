ea1dac7c6fd9d6472cccc73241b848f46a406d8b
Russell Jurney <russell.jurney@gmail.com>
2018-05-01 15:44:33 -0700

    Removed print statement

--- ch04/web/on_time_flask_template.py
+++ ch04/web/on_time_flask_template.py
@@ -109,7 +109,6 @@ def search_flights():
   end = request.args.get('end') or config.RECORDS_PER_PAGE
   end = int(end)
   
-  print(request.args)
   # Navigation path and offset setup
   nav_path = strip_place(request.url)
   nav_offsets = get_navigation_offsets(start, end, config.RECORDS_PER_PAGE)
