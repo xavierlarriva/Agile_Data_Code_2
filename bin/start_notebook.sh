339788f9941cea973826c8354521748f38f78495
Russell Jurney <russell.jurney@gmail.com>
2019-10-29 12:45:56 -0700

    Now stopping existing notebooks first

--- bin/start_notebook.sh
+++ bin/start_notebook.sh
@@ -1,9 +1,9 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 cd $PROJECT_HOME
 
 # Stop all existing jupyter notebooks
-ps aux|grep -i jupyter|grep -v grep|tr -s ' '|cut -d ' ' -f2|xargs sudo kill -9
+ps aux | grep -i jupyter | grep -v grep | tr -s ' ' | cut -d ' ' -f2 | xargs sudo kill -9
 
 # Start a new Jupyter Notebook
 nohup jupyter notebook --ip=0.0.0.0 --NotebookApp.token= --allow-root --no-browser &
