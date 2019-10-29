df78b486e324ded89daf418be1eef0e9c40d517e
Russell Jurney <russell.jurney@gmail.com>
2019-10-29 12:44:39 -0700

    Now stopping existing notebooks first

--- bin/start_notebook.sh
+++ bin/start_notebook.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
-cd /home/ubuntu/Agile_Data_Code_2
+cd $PROJECT_HOME
 
+# Stop all existing jupyter notebooks
+ps aux|grep -i jupyter|grep -v grep|tr -s ' '|cut -d ' ' -f2|xargs sudo kill -9
+
+# Start a new Jupyter Notebook
 nohup jupyter notebook --ip=0.0.0.0 --NotebookApp.token= --allow-root --no-browser &
 
 echo "Jupyter notebook started!"
