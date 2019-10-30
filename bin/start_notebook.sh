68eb434a14aaa3c5691d5d268a1c09f32f0c10de
Russell Jurney <russell.jurney@gmail.com>
2019-10-30 16:27:01 -0700

    Feed sudo the path to jupyter

--- bin/start_notebook.sh
+++ bin/start_notebook.sh
@@ -6,6 +6,6 @@ cd $PROJECT_HOME
 ps aux | grep -i jupyter | grep -v grep | tr -s ' ' | cut -d ' ' -f2 | xargs -I {} sudo kill -9 {}
 
 # Start a new Jupyter Notebook
-nohup jupyter notebook --ip=0.0.0.0 --NotebookApp.token= --allow-root --no-browser &
+nohup `which jupyter` notebook --ip=0.0.0.0 --NotebookApp.token= --allow-root --no-browser &
 
 echo "Jupyter notebook started!"
