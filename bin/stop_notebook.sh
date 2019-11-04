bb628c35f525d125d2de7ee3d98f766e41fba29c
Russell Jurney <russell.jurney@gmail.com>
2019-11-04 07:38:42 +0100

    Now not killing grep itself

--- bin/stop_notebook.sh
+++ bin/stop_notebook.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-ps aux|grep jupyter|tr -s ' '|cut -d ' ' -f2|xargs kill -9
+ps aux| grep jupyter | grep -v grep | tr -s ' '| cut -d ' ' -f2 | xargs -I {} kill -9 {}
 
 echo "Killed Jupyter Notebook!"
